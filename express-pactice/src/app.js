e2fd71d5a925f3d265aef33e7f7dc5eeeeaa290b
de kraftman <olamideadepegba@gmail.com>
2022-05-30 18:59:18 +0100

    trying to update users

--- express-pactice/src/app.js
+++ express-pactice/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-const usersRoute = require("./routes/users");
 
 const app = express();
 
@@ -21,7 +20,6 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(usersRoute);
 
 app.listen(PORT, async () => {
   await db.sequelize.sync({ alter: true });
@@ -34,4 +32,104 @@ const users = [
   { name: "Robo", age: 43 },
 ];
 */
+app.get("/users", async (req, res) => {
+  let users = await db.User.findAll();
+
+  res.json(users);
+  
+});
+  
+//We are trying to validate the value of the data before posting it to our database
+
+app.post("/users", async (req, res, next) => {
+  const {error} = userValidation(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  } else {
+    console.log('Data is being saved to express db');
+    res.status(200).json({message: "data is saved"});
+  };
+
+  // After validation, Now we push
+ 
+ // users.push(req.body);
+
+  // We have successfully pushed, Now we create the data in our database
+
+  await db.User.create(req.body)
+    .then(() => {
+      res.end();
+    })
+    .catch((error) => {
+      res.status(404).end();
+    });
+});
+
+app.get("/users/:name", async (req, res) => {
+  const { name } = req.params;
+  const user = await db.User.findOne({
+    where: { name: name}
+  });
+
+  if(user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.get("/users/age/:age", async (req, res) => {
+  const { age } = req.params;
+  const user = await db.User.findAll({
+    where: {
+      age: {
+        [Op.lte]: age,
+      },
+    },
+  });
+
+  
+  if(user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.get("/users/id/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await db.User.findAll({
+    where: {
+      id: {
+        [Op.gte]: id,
+      },
+    },
+  });
+
+  
+  if(user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.put('/users', async (req, res) => {
+  const usersId = req.params;
+  const { name , age, sex } = req.body;
+
+  const uDate = await db.User.update({ name: name, age: age, sex: sex}, {
+    where: {
+      users: usersId,
+    }
+  });
+
+  if(uDate) {
+    res.status(200).send(uDate);
+  } else {
+    res.status(404).send("Not found");
+  }
+
+});
 
